52b1713eb20cb60b7636cad9d7ed37243df66fc4
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-27 14:41:20 -0300

    feat: :sparkles: add forms controller at the forms router

--- src/routes/formsRouter.ts
+++ src/routes/formsRouter.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import formsController from '../controllers/formsController';
 
 const formsRouter = Router();
+
+formsRouter.post('/', formsController.create);
 
 export default formsRouter;
