cb8b007d12922d92933338a553a13a88fb415b2d
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-27 13:41:37 -0300

    refactor: :recycle: import routes for project

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import handlerError from './middlewares/handlerError';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 app.use(handlerError);
 
 export default app;
