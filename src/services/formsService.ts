ec00f753b90577a30f99f7a6e3a24504ee4794ac
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-27 17:43:09 -0300

    feat: :sparkles: create service function for format date

--- src/services/formsService.ts
+++ src/services/formsService.ts
@@ -1,5 +1,6 @@
 import formsRepository from '../repositories/formsRepository';
 import { Forms } from '../types/formsInterface';
+import transformDate from '../utils/transformDate';
 
 async function checkEmailExist(email: string) {
   const emailForm = await formsRepository.getFormByEmail(email);
@@ -18,9 +19,28 @@ async function createForm(form: Forms) {
   await formsRepository.createForm(form);
 }
 
+async function formatDate(initialDate: string, finalDate: string) {
+  const startDate = transformDate(initialDate);
+  const endDate = transformDate(finalDate);
+
+  if (startDate > endDate) {
+    throw {
+      type: 'unprocessable_entity',
+      message: 'A data inicial deve ser menor que a data final',
+    };
+  }
+
+  return { startDate, endDate };
+}
+
+async function getFormsByDate(startDate: string, endDate: string) {
+  await formsRepository.getFormsByDate(startDate, endDate);
+}
 const formsService = {
   checkEmailExist,
   createForm,
+  formatDate,
+  getFormsByDate,
 };
 
 export default formsService;
