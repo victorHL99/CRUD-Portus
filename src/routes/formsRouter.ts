7e1da8a7e81656fc0bbf5e274cac63eebc654a2c
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-27 15:35:46 -0300

    refactor: :recycle: add middlewares at the endpoint

--- src/routes/formsRouter.ts
+++ src/routes/formsRouter.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import validateSchema from '../middlewares/schemaValidate';
 import formsController from '../controllers/formsController';
+import formsSchema from '../schemas/formsSchema';
 
 const formsRouter = Router();
 
-formsRouter.post('/', formsController.create);
+formsRouter.post(
+  '/',
+  validateSchema(formsSchema.create),
+  formsController.create,
+);
 
 export default formsRouter;
