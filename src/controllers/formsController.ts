3656df2007286865f3937123999ec24e41f494d6
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-27 16:30:16 -0300

    feat: :sparkles: create function for insert form at the database

--- src/controllers/formsController.ts
+++ src/controllers/formsController.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from 'express';
+import { Forms } from '../types/formsInterface';
+import formsService from '../services/formsService';
 
 async function createForm(request: Request, response: Response) {
-  // ...
+  const { name, email, cpf, phone } = request.body as Forms;
+
+  await formsService.checkEmailExist(email);
+
+  const form = {
+    name,
+    email,
+    cpf,
+    phone,
+  } as Forms;
+
+  await formsService.createForm(form);
+
+  return response
+    .status(201)
+    .json({ message: 'Formulário criado com sucesso' });
 }
 
 const formsController = {
