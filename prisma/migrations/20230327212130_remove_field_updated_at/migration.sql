9876e3edbd8c1fb9b7477b420044266271041d05
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-27 18:22:21 -0300

    refactor: :recycle: remove update_at

---
+++ prisma/migrations/20230327212130_remove_field_updated_at/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at` on the `Forms_Answers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Forms_Answers" DROP COLUMN "updated_at";
--- src/types/formsInterface.ts
+++ src/types/formsInterface.ts
@@ -1,3 +1,3 @@
 import { Forms_Answers } from '@prisma/client';
 
-export type Forms = Omit<Forms_Answers, 'id' | 'created_at' | 'updated_at'>;
+export type Forms = Omit<Forms_Answers, 'id' | 'created_at'>;
