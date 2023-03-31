e55cabd098cbab1822af8bbe93a21bf22919ef5a
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-31 16:17:18 -0300

    feat: :sparkles: add limiter for app

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import cors from 'cors';
 import 'express-async-errors';
 import handlerError from './middlewares/handlerError';
 import routes from './routes';
+import limiter from 'middlewares/rateLimit';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(limiter);
 app.use(handlerError);
 
 export default app;
